675f049e3dff8f9b278f41d00c1dfa37757c57d9
willy0483 <willyjensen251@gmail.com>
2026-01-10 02:17:35 +0100

    added triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+
 const unsigned int width = 800;
 const unsigned int height = 600;
 
@@ -34,6 +36,52 @@ int main()
 		return -1;
 	}
 
+	// create shaders
+	Shader shader = Shader("src/Shaders/default.vert", "src/Shaders/default.frag");
+
+	float vertices[] = {
+		// positions        // color
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+		0.5f,  -0.5f, 0.0,	0.0f, 1.0f, 0.0f, // bottom right
+		0.0f,  0.5f,  0.0,	0.0f, 0.0f, 1.0f, // top middle
+	};
+
+	unsigned int indices[] = { 0, 1, 2 };
+
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+
+	// genrate
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// bind - data
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// attributes
+	// positions
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, (6 * sizeof(float)), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// unbind
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	shader.use();
+
 	// main loop
 	while(!glfwWindowShouldClose(window))
 	{
@@ -42,11 +90,23 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	// delete objects
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	// delete shaders
+	shader.deleteProgram();
+
+	// clearing all previously allocated GLFW resources
 	glfwTerminate();
 	return 0;
 }
